88a5a3c8c4e54039a780ce8344e08c2bd3d52ddf
stella <sftamesis13@gmail.com>
2023-06-12 16:39:36 -0700

    create controller file

---
+++ app/controllers/game_logs_controller.rb
@@ -0,0 +1,2 @@
+class GameLogsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :profiles, only: [:create, :show, :update, :destroy]
+  resources :games, only: [:index, :create, :show, :destroy]
+  resources :game_logs, only: [:show, :update, :destroy]
+  resources :genre, only: [:index]
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#create'
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   # get '/hello', to: 'application#hello_world'
