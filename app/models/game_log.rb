6a6467ff25e1c198010df87433d64ccde1baa261
stella <sftamesis13@gmail.com>
2023-06-12 16:59:33 -0700

    fix syntax errors

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -3,10 +3,10 @@ class GameLog < ApplicationRecord
   belongs_to :user
 
   STATUS_OPTIONS = {
-    0 => "Not Started", 
+    0 => "Not Started",
     1 => "In Progress",
-    2 => "Did not Finish"
-    3 => "Complete"
+    2 => "Did not Finish",
+    3 => "Complete",
     4 => "Wishlist"
   }
 
@@ -15,10 +15,9 @@ class GameLog < ApplicationRecord
   validates :date_started, allow_blank: true
   validates :date_stopped, allow_blank: true
   validates :date_completed, allow_blank: true
-  validates :play_time, numericality { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :play_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def display_status
     STATUS_OPTIONS[status]
   end
-
 end
