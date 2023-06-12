76061caaddab0b6cd2272f6c40a162cc22be10c0
stella <sftamesis13@gmail.com>
2023-06-12 16:21:41 -0700

    create json data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+
+# Read the contents of the JSON file
+json_data = File.read('db/IGDB_data.json')
+
+# Parse the JSON data
+data = JSON.parse(json_data)
+
+# Iterate over the data and create records
+data.each do |item|
+  # Extract relevant attributes
+  name = item['name']
+  cover_url = item['cover']['url'] if item['cover']
+  release_date = Time.at(item['first_release_date']) if item['first_release_date']
+  involved_company = item['involved_companies']&.map { |company| company['company']['name'] }
+  player_perspective = item['player_perspectives']&.map { |perspective| perspective['name'] }
+  aggregated_rating = item['aggregated_rating']
+  aggregated_rating_count = item['aggregated_rating_count']
+  summary = item['summary']
+  genre_name = item['genres']&.first&.dig('name')
+  platform_names = item['platforms']&.map { |platform| platform['name'] }
+
+
+  # Find the genre by name or create a new one
+  genre = Genre.find_or_create_by(name: genre_name)
+
+  # Create a record using the extracted attributes
+  Game.create(
+    name: name,
+    cover: cover_url,
+    platform: platform_names,  # Update the platform attribute
+    release_date: release_date,
+    involved_company: involved_company,
+    player_perspective: player_perspective,
+    aggregated_rating: aggregated_rating,
+    aggregated_rating_count: aggregated_rating_count,
+    summary: summary,
+    genre: genre
+  )
+end
+
+puts 'Seeding Completed!'
