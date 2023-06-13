b4973c6f787bea81d34e9460d372cd43f952466f
stella <sftamesis13@gmail.com>
2023-06-13 13:25:16 -0700

    create actions and tested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
     skip_before_action :authorize, only: [:create] 
 
+    # LOGIN
     def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
     end
 
+    #LOGOUT
     def destroy
+        session.delete :user_id
+        head :no_content
     end
+    
 
 end
