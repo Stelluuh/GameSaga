c418070ddfb74711e4250c10eadd856b765f5a95
stella <sftamesis13@gmail.com>
2023-06-13 09:44:18 -0700

    move validates status to bottom

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -10,15 +10,15 @@ class GameLog < ApplicationRecord
     4 => "Wishlist"
   }
 
-  validates :status, presence: true, inclusion: { in: STATUS_OPTIONS.keys }
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :date_started, allow_blank: true
   validates :date_stopped, allow_blank: true
   validates :date_completed, allow_blank: true
   validates :play_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :status, presence: true, inclusion: { in: STATUS_OPTIONS.keys }
 
   def display_status
     STATUS_OPTIONS[status]
   end
-
 end
+
