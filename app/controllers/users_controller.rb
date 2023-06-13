e722703a48e555663d9220b30e65981186bb58f9
stella <sftamesis13@gmail.com>
2023-06-13 13:24:30 -0700

    remove find user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
-    skip_before_action :authorize, only: [:create] 
+    skip_before_action :authorize, only: [:create, :index] 
     
+    def index
+        users = User.all
+        render json: users
+    end
+
+
     # LOGIN
     def show
         render json: @current_user 
