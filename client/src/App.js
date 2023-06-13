d6123c427cdb12b42755e1e6e036d3d82bfad53a
stella <sftamesis13@gmail.com>
2023-06-13 10:03:05 -0700

    uncomment helloworld example

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home'
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
       <div className="App">
