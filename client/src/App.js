28b6d146824e40149a960dc62fe7920900f8cc4e
stella <sftamesis13@gmail.com>
2023-06-14 10:04:47 -0700

    modified to handle signup component

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,17 @@
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from './context/AuthContext'
 import Home from './Components/Home'
+import Signup from './Components/Signup'
+
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // useEffect(() => {
-  //   fetch("/hello")
-  //     .then((r) => r.json())
-  //     .then((data) => setCount(data.count));
-  // }, []);
 
   return (
       <div className="App">
         <UserProvider>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </UserProvider>
       </div>
