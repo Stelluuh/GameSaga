32fb2a9d370d6ad7ca5be276fba1c308c9e676a4
stella <sftamesis13@gmail.com>
2023-06-14 10:05:45 -0700

    modified to handle signup logic

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/AuthContext'
 
 const Signup = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [errorsList, setErrorsList] = useState([])
+  const { handleSignup } = useContext(UserContext)
+
   return (
     <div>
       
