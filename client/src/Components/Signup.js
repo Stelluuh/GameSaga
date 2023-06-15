1eb03ca64f10cd841d5c42edd271b53360070fe9
stella <sftamesis13@gmail.com>
2023-06-15 11:06:34 -0700

    fix error css

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -21,14 +21,14 @@ const Signup = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password,
-          password_confirmation: passwordConfirmation
-        })
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation
       })
+    })
       .then(response => response.json())
       .then(user => {
         if (!user.errors) {
@@ -44,50 +44,52 @@ const Signup = () => {
           setPasswordConfirmation('')
         }
       })
-    }
+  }
 
   return (
     <div className="signup-container">
-    <form className="signup-form" onSubmit={handleSubmit}>
-      <h1>Signup</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        id="passwordConfirmation"
-        value={passwordConfirmation}
-        onChange={(e) => setPasswordConfirmation(e.target.value)}
-        required
-      />
-      <button type="submit">Sign Up</button>
-    </form>
-    <ul className="errors-list">
-      {errorsList}
-    </ul>
-  </div>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h1>Signup</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          id="passwordConfirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+        {errorsList.length > 0 && (
+          <ul className="errors-list">
+            {errorsList}
+          </ul>
+        )}
+      </form>
+    </div>
   )
 }
 
