8630036396c922c13b8afee8b42be01f5e19450f
stella <sftamesis13@gmail.com>
2023-06-19 13:52:04 -0700

    Handle user profiles without setup. If user did not set up their profile, they are still directed to their profile page but only their basic info is shown

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -7,15 +7,13 @@ const Profile = () => {
   const [profile, setProfile] = useState({});
   const { user, isLoggedIn } = useContext(UserContext);
 
-
   useEffect(() => {
     fetch(`/profiles/${user.id}`)
       .then(response => response.json())
       .then(userProfile => {
         setProfile(userProfile);
-        // console.log(userProfile);
       });
-  }, []);
+  }, [user.id]);
 
   const handleEditProfile = () => {
     // console.log('edit profile');
@@ -29,16 +27,15 @@ const Profile = () => {
           <div className="profile-details">
             <p><label>Username:</label> {user.username}</p>
             <p><label>Email:</label> {user.email}</p>
-            <p><label>Name:</label> {profile.name}</p>
-            <p><label>Age:</label> {profile.age}</p>
-            {/* <image><label>Avatar:</label>{profile.avatar}</image> */}
-            <p><label>Avatar:</label> {profile.avatar}</p>
-            <p><label>Bio:</label> {profile.bio}</p>
-            <p><label>Total Games Played:</label> {profile.total_games_played}</p>
-            <p><label>Favorite Genre:</label> {profile.favorite_genre}</p>
-            <p><label>Hours Played:</label> {profile.hours_played}</p>
+            {profile && profile.name && <p><label>Name:</label> {profile.name}</p>}
+            {profile && profile.age && <p><label>Age:</label> {profile.age}</p>}
+            {profile && profile.avatar && <p><label>Avatar:</label> {profile.avatar}</p>}
+            {profile && profile.bio && <p><label>Bio:</label> {profile.bio}</p>}
+            {profile && profile.total_games_played && <p><label>Total Games Played:</label> {profile.total_games_played}</p>}
+            {profile && profile.favorite_genre && <p><label>Favorite Genre:</label> {profile.favorite_genre}</p>}
+            {profile && profile.hours_played && <p><label>Hours Played:</label> {profile.hours_played}</p>}
           </div>
-          <button onClick={() => <EditProfile />}>Edit Profile</button>
+          <button onClick={handleEditProfile}>Edit Profile</button>
         </div>
       </div>
     );
