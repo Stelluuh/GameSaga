483891638da44bd8e5d1b076f8cca4f9837dfd6a
stella <sftamesis13@gmail.com>
2023-06-20 14:17:12 -0700

    remove profile state and use the user data from AuthContext file

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,22 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../context/AuthContext';
-import EditProfile from './EditProfile';
+import ProfileForm from './ProfileForm';
 import '../Styles/Profile.css';
 
 const Profile = () => {
-  const [profile, setProfile] = useState({})
-  const [editing, setEditing] = useState(false)
   const { user, isLoggedIn } = useContext(UserContext);
-
-  useEffect(() => {
-    fetch(`/profiles/${user.id}`)
-      .then(response => response.json())
-      .then(userProfile => {
-        setProfile(userProfile)
-        console.log(userProfile)
-      });
-  }, [user.id]);
-
+  const [editing, setEditing] = useState(false);
 
   if (!isLoggedIn) {
     return (
@@ -27,34 +16,76 @@ const Profile = () => {
         </div>
       </div>
     );
-  } else if (editing) { //if logged in and editing is true
-    return (
-      <div className="profile-container">
-        <EditProfile profile={profile} setProfile={setProfile} setEditing={setEditing} />
-      </div>
-    );
-
-  } else { // if logged in and not editing:
+  } else if (editing) {
     return (
       <div className="profile-container">
         <div className="profile-card">
           <h2>Account Details</h2>
           <div className="account-details">
-            <p><label>Username:</label> {user.username}</p>
-            <p><label>Email:</label> {user.email}</p>
+            <p>
+              <label>Username:</label> {user.username}
+            </p>
+            <p>
+              <label>Email:</label> {user.email}
+            </p>
+          </div>
+          <hr />
+          <br />
+          <h2>Profile Details</h2>
+        </div>
+        <div className="profile-form-container">
+          <ProfileForm setEditing={setEditing} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="profile-container">
+        <div className="profile-card">
+          <h2>Account Details</h2>
+          <div className="account-details">
+            <p>
+              <label>Username:</label> {user.username}
+            </p>
+            <p>
+              <label>Email:</label> {user.email}
+            </p>
           </div>
           <hr />
           <br />
           <h2>Profile Details</h2>
           <div className="profile-details">
-            {profile && profile.avatar && <img src={profile.avatar} alt="Avatar" />}
-            <p><label>Name:</label>{profile && profile.name && <span> {profile.name}</span>}</p>
-            <p><label>Age:</label>{profile && profile.age && <span> {profile.age}</span>}</p>
-            <p><label>Bio:</label>{profile && profile.bio && <span> {profile.bio}</span>}</p>
-            <p><label>Total Games Played:</label>{profile && profile.total_games_played && <span> {profile.total_games_played}</span>}</p>
-            <p><label>Favorite Genre:</label>{profile && profile.favorite_genre && <span> {profile.favorite_genre}</span>}</p>
-            <p><label>Hours Played:</label>{profile && profile.hours_played && <span> {profile.hours_played}</span>}</p>
+            {user && user.avatar && <img src={user.avatar} alt="Avatar" />}
+            {user && (
+              <>
+                <p>
+                  <label>Name:</label>
+                  <span>{user.name}</span>
+                </p>
+                <p>
+                  <label>Age:</label>
+                  <span>{user.age}</span>
+                </p>
+                <p>
+                  <label>Bio:</label>
+                  <span>{user.bio}</span>
+                </p>
+                <p>
+                  <label>Total Games Played:</label>
+                  <span>{user.total_games_played}</span>
+                </p>
+                <p>
+                  <label>Favorite Genre:</label>
+                  <span>{user.favorite_genre}</span>
+                </p>
+                <p>
+                  <label>Hours Played:</label>
+                  <span>{user.hours_played}</span>
+                </p>
+              </>
+            )}
           </div>
+
           <button onClick={() => setEditing(true)}>Edit Profile</button>
           <hr />
         </div>
