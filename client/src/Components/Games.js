555ef096874aab2510dc3438887f22c55fce0f9b
stella <sftamesis13@gmail.com>
2023-06-21 13:16:26 -0700

    create games component

---
+++ client/src/Components/Games.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/AuthContext'
+
+const Games = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Games
