64706259cc886114460f2e259efc77fe7e7c9fb4
stella <sftamesis13@gmail.com>
2023-06-22 14:43:08 -0700

    install bootstrap and FixedSizedList

--- client/src/Components/Games.js
+++ client/src/Components/Games.js
@@ -2,11 +2,37 @@ import React, { useContext } from 'react'
 import { UserContext } from '../context/AuthContext'
 
 const Games = () => {
+  const { isLoggedIn, games } = useContext(UserContext)
+
+  console.log(games)
+
+  
+  if (!isLoggedIn) {
+    return <h3>Please login to view your games.</h3>
+  } else {
   return (
     <div>
-      
+      <h1>Browse Games</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Game</th>
+            <th>Genre</th>
+            <th>Platform</th>
+          </tr>
+        </thead>
+        <tbody>
+          {games.map((game) => (
+            <tr key={game.id}>
+              <td>{game.name}</td>
+              <td>{game.genre}</td>
+              <td>{game.platform}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>      
     </div>
-  )
+  )}
 }
 
 export default Games
