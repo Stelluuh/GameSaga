4bfdbe85053bf2c2e89f86ec8046530c4f9d1fa9
stella <sftamesis13@gmail.com>
2023-06-22 14:45:19 -0700

    use bootstrap css

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../context/AuthContext';
 import { NavLink, useNavigate } from 'react-router-dom';
-import '../Styles/NavBar.css';
+
 
 const NavBar = () => {
   const { isLoggedIn, logout, user } = useContext(UserContext);
@@ -21,32 +21,84 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="navbar">
-      {isLoggedIn ? (
-        <>
-          <NavLink to={`/profiles/${user.username}`} className="navbar-link">
-            <button className="navbar-button">My Profile</button>
-          </NavLink>
-          <NavLink to="/games" className="navbar-link">
-            <button className="navbar-button">Browse Games</button>
-          </NavLink>
-          <button className="navbar-button" onClick={handleLogout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <NavLink exact to="/" className="navbar-link">
-            <button className="navbar-button">Home</button>
-          </NavLink>
-          <NavLink to="/login" className="navbar-link">
-            <button className="navbar-button">Login</button>
-          </NavLink>
-          <NavLink to="/signup" className="navbar-link">
-            <button className="navbar-button">Signup</button>
-          </NavLink>
-        </>
-      )}
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-right">
+      <div className="container">
+        <NavLink exact to="/" className="navbar-brand">
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item">
+                  <NavLink
+                    to={`/profiles/${user.username}`}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    My Profile
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/games"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Browse Games
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link btn btn-link" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    to="/"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/login"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/signup"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Signup
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
