6a0fad57f5f352744af6f3f80a6c0544ae2e6731
stella <sftamesis13@gmail.com>
2023-07-01 14:16:18 -0700

    define routes and actions for library_games

--- app/controllers/library_games_controller.rb
+++ app/controllers/library_games_controller.rb
@@ -1,2 +1,33 @@
 class LibraryGamesController < ApplicationController
+    before_action :authorize
+
+    def index
+        library_games = @current_user.library_games
+        render.json: library_games
+    end
+
+    def show
+        render.json: @library_games
+    end
+
+    def create
+        library_game = @current_user.library_games.create!(library_game_params)
+        render json: library_game, status: :created
+    end
+
+    def update
+        @library_game.update!(library_game_params)
+        render json: @library_game, status: :accepted
+    end
+
+    def destroy
+        @library_game.destroy
+        head :no_content
+    end
+
+    private
+
+    def library_game_params
+        params.permit(:status)
+    end
 end
