df7b6e3cf84b61650b9d11a5a4bef12575a2f5d0
stella <sftamesis13@gmail.com>
2023-07-01 18:07:55 -0700

    remove library_games logic

--- app/models/library_game.rb
+++
@@ -1,4 +0,0 @@
-class LibraryGame < ApplicationRecord
-  belongs_to :user
-  belongs_to :game
-end
--- app/serializers/library_game_serializer.rb
+++
@@ -1,5 +0,0 @@
-class LibraryGameSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user
-  has_one :game
-end
