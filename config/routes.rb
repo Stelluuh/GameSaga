68966674dc69b0d68bff117b9b4ecc7856ed4852
stella <sftamesis13@gmail.com>
2023-07-05 09:58:12 -0700

    include create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :profiles, only: [:index, :create, :show, :update]
   resources :games, only: [:index, :create, :show, :update]
-  resources :game_logs, only: [:show, :update, :destroy]
+  resources :game_logs, only: [:create, :show, :update, :destroy]
   resources :genres, only: [:index, :show, :create]
   resources :users, only: [:index, :show, :create, :update, :destroy]
 
