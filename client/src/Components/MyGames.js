b6ea24dcb17b790f4a41bd71537d98c3ffc15864
stella <sftamesis13@gmail.com>
2023-07-05 11:40:57 -0700

    refactor

--- client/src/Components/MyGames.js
+++ client/src/Components/MyGames.js
@@ -3,21 +3,24 @@ import { UserContext } from '../context/AuthContext'
 import MyGameCard from './MyGameCard'
 
 const MyGames = () => {
-    const { user } = useContext(UserContext)
+    const { user, isLoggedIn } = useContext(UserContext)
     console.log(user)
 
     const gameList = user?.games?.map(game => {
         return <MyGameCard key={game.id} game={game} />
     })
 
-    
-
-  return (
-    <div>
-      <h1>My Games!</h1>
-      {gameList}
-    </div>
-  )
+    return (
+      <div>
+        {isLoggedIn ? (
+          <>
+          {gameList}
+          </>
+          ) : (
+            <h3>Please Login or Signup</h3>
+          )}
+      </div>
+    )
 }
 
 export default MyGames
