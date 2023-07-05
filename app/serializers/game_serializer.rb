09d9297e811286328f2a6d5675d82d15f4056312
stella <sftamesis13@gmail.com>
2023-07-05 13:26:20 -0700

    remove unneccessary serializers

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,6 +2,4 @@ class GameSerializer < ActiveModel::Serializer
   attributes :id, :cover, :name, :platforms, :release_date, :involved_company, :player_perspective, :aggregated_rating, :aggregated_rating_count, :summary, :artworks, :screenshots, :keywords, :genre_id, :genre, :summary, :websites, :game_logs
 
   belongs_to :genre
-  has_many :game_logs
-  has_many :users, through: :game_logs
 end
