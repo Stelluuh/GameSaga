da9d0d7a631cd0fcd49736091d1461e5b3f7c2cb
stella <sftamesis13@gmail.com>
2023-07-05 09:53:38 -0700

    add dependent destroy

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -2,11 +2,11 @@ class GameLog < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  validates :status, inclusion: { in: ['Not Played', 'In Progress', 'Abandoned', 'Complete', 'Wishlist'] }
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }, allow_nil: true
-  validates :date_started, presence: true
-  validates :date_stopped, presence: true, if: :completed?
-  validates :date_completed, presence: true, if: :completed?
+  # validates :status, inclusion: { in: ['Not Played', 'In Progress', 'Abandoned', 'Complete', 'Wishlist'] }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
+  # validates :date_started, presence: true
+  # validates :date_stopped, presence: true, if: :completed?
+  # validates :date_completed, presence: true, if: :completed?
   validates :play_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   private
