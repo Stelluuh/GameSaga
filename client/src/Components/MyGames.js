7de1285e27e5bc7a1577ab8e34b1ab46de1d515d
stella <sftamesis13@gmail.com>
2023-07-11 12:20:11 -0700

    add indicator that user has nothing in their library

--- client/src/Components/MyGames.js
+++ client/src/Components/MyGames.js
@@ -1,26 +1,30 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../context/AuthContext'
-import MyGameCard from './MyGameCard'
+import React, { useContext } from 'react';
+import { UserContext } from '../context/AuthContext';
+import MyGameCard from './MyGameCard';
 
 const MyGames = () => {
-    const { user, isLoggedIn } = useContext(UserContext)
-    console.log(user)
+  const { user, isLoggedIn } = useContext(UserContext);
+  console.log(user);
 
-    const gameList = user?.games?.map(game => {
-        return <MyGameCard key={game.id} game={game} />
-    })
+  const gameList = user?.games?.map(game => {
+    return <MyGameCard key={game.id} game={game} />;
+  });
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <>
-          {gameList}
-          </>
+  return (
+    <div>
+      {isLoggedIn ? (
+        <>
+          {gameList.length > 0 ? (
+            gameList
           ) : (
-            <h3>Please Login or Signup</h3>
+            <h3 style={{ color: 'red' }}>You currently have no games in your library.</h3>
           )}
-      </div>
-    )
-}
+        </>
+      ) : (
+        <h3>Please Login or Signup</h3>
+      )}
+    </div>
+  );
+};
 
-export default MyGames
+export default MyGames;
