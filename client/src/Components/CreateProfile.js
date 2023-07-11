630f8eab56c8ed61f23c6e998659dd5ff9fea563
stella <sftamesis13@gmail.com>
2023-07-11 10:18:00 -0700

    remove logic for manual addition of total games played and hours played

--- client/src/Components/CreateProfile.js
+++ client/src/Components/CreateProfile.js
@@ -5,15 +5,16 @@ import '../Styles/CreateProfile.css'
 
 
 const CreateProfile = () => {
+    const { addProfile, genres, user, isLoggedIn } = useContext(UserContext)
+    
     const [name, setName] = useState('')
     const [age, setAge] = useState('')
     const [avatar, setAvatar] = useState('https://emedia1.nhs.wales/HEIW2/cache/file/F4C33EF0-69EE-4445-94018B01ADCF6FD4_medium.png')
     const [bio, setBio] = useState('')
-    const [totalGamesPlayed, setTotalGamesPlayed] = useState(0)
+    // const [totalGamesPlayed, setTotalGamesPlayed] = useState(0)
     const [favoriteGenre, setFavoriteGenre] = useState('')
-    const [hoursPlayed, setHoursPlayed] = useState(0)
+    // const [hoursPlayed, setHoursPlayed] = useState(0)
 
-    const { addProfile, genres, user, isLoggedIn } = useContext(UserContext)
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
@@ -24,9 +25,9 @@ const CreateProfile = () => {
             age: age,
             avatar: avatar,
             bio: bio,
-            total_games_played: totalGamesPlayed,
+            // total_games_played: totalGamesPlayed,
             favorite_genre: favoriteGenre,
-            hours_played: hoursPlayed,
+            // hours_played: hoursPlayed,
         }
         addProfile(newProfile)
         navigate(`/profiles/${user.username}`)
@@ -50,8 +51,8 @@ const CreateProfile = () => {
             <input type="text" value={avatar} onChange={(e) => setAvatar(e.target.value)} />
             <label> Bio: </label>
             <input type="text" value={bio} onChange={(e) => setBio(e.target.value)} />
-            <label> Total Games Played: </label>
-            <input type="number" value={totalGamesPlayed} onChange={(e) => setTotalGamesPlayed(e.target.value)} />
+            {/* <label> Total Games Played: </label> */}
+            {/* <input type="number" value={totalGamesPlayed} onChange={(e) => setTotalGamesPlayed(e.target.value)} /> */}
             <label> Favorite Genre: </label>
             <select value={favoriteGenre} onChange={(e) => setFavoriteGenre(e.target.value)}>
                 <option value="N/A">Select Genre</option>
@@ -61,8 +62,8 @@ const CreateProfile = () => {
                     </option>
                  ))}
           </select>
-            <label> Hours Played: </label>
-            <input type="number" value={hoursPlayed} onChange={(e) => setHoursPlayed(e.target.value)} />
+            {/* <label> Hours Played: </label> */}
+            {/* <input type="number" value={hoursPlayed} onChange={(e) => setHoursPlayed(e.target.value)} /> */}
             <br />
             <button type="submit">Submit</button>
         </form>
