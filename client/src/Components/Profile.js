f3b668e5c7759e143d44b23d880fb98c0ebd2f55
stella <sftamesis13@gmail.com>
2023-07-11 10:14:51 -0700

    remove manual addition of total games played and hours played

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,11 +1,60 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../context/AuthContext';
-import EditProfile from './EditProfile';
-import '../Styles/Profile.css';
+import React, { useContext, useState, useEffect } from 'react'
+import { UserContext } from '../context/AuthContext'
+import EditProfile from './EditProfile'
+import PieChart from './PieChart'
+import { CategoryScale } from 'chart.js'
+import Chart from 'chart.js/auto'
+import '../Styles/Profile.css'
+
+Chart.register(CategoryScale)
 
 const Profile = () => {
   const { user, isLoggedIn, deleteAccount } = useContext(UserContext)
   const [editing, setEditing] = useState(false)
+  const [chartData, setChartData] = useState(null)
+  
+  useEffect(() => {
+    if (user && user.games) {
+      const games = user.games;
+      const genres = games.map(game => game.genre);
+      // uniqueGenres is an array of unique genres. It's used to create the chart labels.
+      const uniqueGenres = [...new Set(genres)]; //refactored from https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+      // ...new Set(genres) is used to remove duplicate genres from the genres array.
+      const chartData = {
+        labels: [],
+        datasets: [{ data: [], backgroundColor: [], borderColor: [] }]
+      };
+  
+      games.forEach(game => {
+        const genre = game.genre.name;
+  
+        // Check if the genre already exists in the chart data
+        const existingGenreIndex = chartData.labels.indexOf(genre);
+        if (existingGenreIndex !== -1) { // -1 means it doesn't exist
+          // Update the existing data value for the genre
+          chartData.datasets[0].data[existingGenreIndex]++;
+        } else {
+          // Add a new genre to the chart data
+          chartData.labels.push(genre);
+          chartData.datasets[0].data.push(1);
+          chartData.datasets[0].backgroundColor.push(getRandomColor());
+          chartData.datasets[0].borderColor.push(getRandomColor());
+        }
+      });
+  
+      setChartData(chartData);
+    }
+  }, [user]);
+  
+  // Function to generate a random color
+  // Refactored from https://stackoverflow.com/questions/1484506/random-color-generator
+  function getRandomColor() {
+    // Math.random() = random number between 0 and 1
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16); //16777215 is the decimal value of the highest hex color value (FFFFFF)
+    return "#" + randomColor;
+    console.log(randomColor)
+  }
+  
 
   if (!isLoggedIn) {
     return (
@@ -88,6 +137,7 @@ const Profile = () => {
           
           <button onClick={() => setEditing(true)}>Edit Profile</button>
           <hr />
+          {chartData && <PieChart chartData={chartData} />}
         </div>
       </div>
     );
