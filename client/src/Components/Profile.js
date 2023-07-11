156aa298c0d9ebcfa1c40c70891737b46b1a1bd8
stella <sftamesis13@gmail.com>
2023-07-11 11:18:45 -0700

    refactor to choose from a list of colors instead of generating random colors

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -15,30 +15,43 @@ const Profile = () => {
   
   useEffect(() => {
     if (user && user.games) {
-      const games = user.games;
-      const genres = games.map(game => game.genre);
+      const games = user.games.filter(game => game.status !== 'Wishlist' && game.status !== 'Not Played')
+      const genres = games.map(game => game.genre)
       // uniqueGenres is an array of unique genres. It's used to create the chart labels.
       const uniqueGenres = [...new Set(genres)]; //refactored from https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
       // ...new Set(genres) is used to remove duplicate genres from the genres array.
       const chartData = {
         labels: [],
-        datasets: [{ data: [], backgroundColor: [], borderColor: [] }]
+        datasets: [
+          {
+            data: [],
+            backgroundColor: [],
+            borderColor: []
+          }
+        ]
       };
   
       games.forEach(game => {
-        const genre = game.genre.name;
+        const genre = game.genre.name
   
         // Check if the genre already exists in the chart data
-        const existingGenreIndex = chartData.labels.indexOf(genre);
-        if (existingGenreIndex !== -1) { // -1 means it doesn't exist
+        const existingGenreIndex = chartData.labels.indexOf(genre)
+        if (existingGenreIndex !== -1) {
           // Update the existing data value for the genre
-          chartData.datasets[0].data[existingGenreIndex]++;
+          chartData.datasets[0].data[existingGenreIndex]++
         } else {
           // Add a new genre to the chart data
-          chartData.labels.push(genre);
-          chartData.datasets[0].data.push(1);
-          chartData.datasets[0].backgroundColor.push(getRandomColor());
-          chartData.datasets[0].borderColor.push(getRandomColor());
+          chartData.labels.push(genre)
+          // Add a new data value for the genre
+          chartData.datasets[0].data.push(1)
+          // Choose color from the list based on index
+          const colorIndex = chartData.labels.length - 1
+          chartData.datasets[0].backgroundColor.push(
+            backgroundColor[colorIndex % backgroundColor.length]
+          )
+          chartData.datasets[0].borderColor.push(
+            borderColor[colorIndex % borderColor.length]
+          )
         }
       });
   
@@ -46,14 +59,43 @@ const Profile = () => {
     }
   }, [user]);
   
-  // Function to generate a random color
-  // Refactored from https://stackoverflow.com/questions/1484506/random-color-generator
-  function getRandomColor() {
-    // Math.random() = random number between 0 and 1
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16); //16777215 is the decimal value of the highest hex color value (FFFFFF)
-    return "#" + randomColor;
-    console.log(randomColor)
-  }
+  const backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 99, 255, 0.2)',
+    'rgba(255, 159, 0, 0.2)',
+    'rgba(255, 205, 0, 0.2)',
+    'rgba(0, 128, 0, 0.2)',
+    'rgba(128, 128, 128, 0.2)',
+    'rgba(0, 0, 255, 0.2)',
+    'rgba(255, 0, 0, 0.2)',
+    'rgba(128, 0, 128, 0.2)',
+    'rgba(0, 255, 0, 0.2)'
+  ];
+  
+  const borderColor = [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+    'rgb(153, 102, 255)',
+    'rgb(255, 99, 255)',
+    'rgb(255, 159, 0)',
+    'rgb(255, 205, 0)',
+    'rgb(0, 128, 0)',
+    'rgb(128, 128, 128)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 0)',
+    'rgb(128, 0, 128)',
+    'rgb(0, 255, 0)'
+  ];
+  
+  
   
 
   if (!isLoggedIn) {
