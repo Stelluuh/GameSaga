cc314404ecc3105e2f18bb8796a7630a786654f3
stella <sftamesis13@gmail.com>
2023-07-12 10:00:32 -0700

    refactor to only show the current users game_logs for each game in their library

--- client/src/Components/MyGameCard.js
+++ client/src/Components/MyGameCard.js
@@ -3,8 +3,8 @@ import { UserContext } from '../context/AuthContext';
 import EditGameLog from './EditGameLog';
 
 const MyGameCard = ({ game }) => {
-  const { name, genre, game_logs } = game;
-  const { deleteGameLog, editGameLog } = useContext(UserContext);
+  const { name, genre } = game;
+  const { deleteGameLog, editGameLog, user } = useContext(UserContext);
   const [editingLogId, setEditingLogId] = useState(null);
 
   const handleDeleteGameLog = (gameLogId) => {
@@ -16,67 +16,62 @@ const MyGameCard = ({ game }) => {
   };
 
   const handleCancelEdit = () => {
-    setEditingLogId(null)
-  }
+    setEditingLogId(null);
+  };
 
   const handleSaveGameLog = (editedLog) => {
-    editGameLog(editedLog)
-    setEditingLogId(null)
-  }
+    editGameLog(editedLog);
+    setEditingLogId(null);
+  };
+
+  const gameLogs = user.game_logs?.filter((log) => log.game_id === game.id);
 
   return (
-    <div className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src={game.cover} alt="Cover" className="img-fluid" />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">Genre: {genre.name}</p>
-            <div>
-              <p>Game Logs:</p>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Status</th>
-                    <th>Rating</th>
-                    <th>Hours Played</th>
-                    <th>Date Start</th>
-                    <th>Date Stopped</th>
-                    <th>Date Completed</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {game_logs?.map((log) => (
-                    <tr key={log.id}>
-                      { editingLogId === log.id ? (
-                        <EditGameLog 
-                          gameLog={log}
-                          onCancel={handleCancelEdit}
-                          onSave={handleSaveGameLog}                         
-                        />
-                      ) : (
-                        <>
-                          <td>{log.status}</td>
-                          <td>{log.rating}</td>
-                          <td>{log.play_time}</td>
-                          <td>{log.date_started}</td>
-                          <td>{log.date_stopped}</td>
-                          <td>{log.date_completed}</td>
-                          <td>
-                            <button onClick={() => handleDeleteGameLog(log.id)}>Delete</button>
-                            <button onClick={() => handleEditGameLog(log.id)}>Edit</button>
-                          </td>
-                        </>
-                      )}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <div className="card bg-dark text-light mb-3">
+      <div className="card-body">
+        <h5 className="card-title text-primary">{name}</h5>
+        <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p>
+        <div>
+          <p className="font-italic">Game Logs:</p>
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th>Status</th>
+                <th>Rating</th>
+                <th>Hours Played</th>
+                <th>Date Start</th>
+                <th>Date Stopped</th>
+                <th>Date Completed</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gameLogs?.map((log) => (
+                <tr key={log.id}>
+                  {editingLogId === log.id ? (
+                    <EditGameLog
+                      gameLog={log}
+                      onCancel={handleCancelEdit}
+                      onSave={handleSaveGameLog}
+                    />
+                  ) : (
+                    <>
+                      <td>{log.status}</td>
+                      <td>{log.rating}</td>
+                      <td>{log.play_time}</td>
+                      <td>{log.date_started}</td>
+                      <td>{log.date_stopped}</td>
+                      <td>{log.date_completed}</td>
+                      <td>
+                        <button className="btn btn-danger" onClick={() => handleDeleteGameLog(log.id)}>Delete</button>
+                        <button className="btn btn-primary" onClick={() => handleEditGameLog(log.id)}>Edit</button>
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
