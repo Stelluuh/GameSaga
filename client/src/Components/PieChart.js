7694c3c13efae37360d81e4c387deed75dac5aa5
stella <sftamesis13@gmail.com>
2023-07-12 14:25:45 -0700

    refactor so that all PieChart logic resides in PieChart Component

--- client/src/Components/PieChart.js
+++ client/src/Components/PieChart.js
@@ -1,22 +1,100 @@
-import React from "react";
-import { Pie } from "react-chartjs-2";
+import React, { useEffect, useState } from 'react';
+import { Pie } from 'react-chartjs-2';
+import { CategoryScale } from 'chart.js';
+import Chart from 'chart.js/auto';
 
-function PieChart({ chartData }) {
+Chart.register(CategoryScale);
+
+const PieChart = ({ games }) => {
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    if (games) {
+      const filteredGames = games.filter(game => game.status !== 'Wishlist' && game.status !== 'Not Played');
+      const genres = filteredGames.map(game => game.genre);
+      const uniqueGenres = [...new Set(genres)];
+
+      const data = {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            backgroundColor: [],
+            borderColor: []
+          }
+        ]
+      };
+
+      uniqueGenres.forEach(genre => {
+        const genreName = genre.name;
+        const count = genres.filter(g => g.name === genreName).length;
+
+        if (!data.labels.includes(genreName)) {
+          data.labels.push(genreName);
+          data.datasets[0].data.push(count);
+          data.datasets[0].backgroundColor.push(backgroundColor[data.labels.length - 1]);
+          data.datasets[0].borderColor.push(borderColor[data.labels.length - 1]);
+        }
+      });
+
+      setChartData(data);
+    }
+  }, [games]);
+
+  const backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 99, 255, 0.2)',
+    'rgba(255, 159, 0, 0.2)',
+    'rgba(255, 205, 0, 0.2)',
+    'rgba(0, 128, 0, 0.2)',
+    'rgba(128, 128, 128, 0.2)',
+    'rgba(0, 0, 255, 0.2)',
+    'rgba(255, 0, 0, 0.2)',
+    'rgba(128, 0, 128, 0.2)',
+    'rgba(0, 255, 0, 0.2)'
+  ];
+  
+  const borderColor = [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+    'rgb(153, 102, 255)',
+    'rgb(255, 99, 255)',
+    'rgb(255, 159, 0)',
+    'rgb(255, 205, 0)',
+    'rgb(0, 128, 0)',
+    'rgb(128, 128, 128)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 0)',
+    'rgb(128, 0, 128)',
+    'rgb(0, 255, 0)'
+  ];
+
   return (
     <div className="chart-container">
-      <h2 style={{ textAlign: "center" }}>Genres</h2>
-      <Pie
-        data={chartData}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: "Different Genres in Library",
+      <h2 style={{ textAlign: 'center', color: 'white' }}>Genres in Library</h2>
+      {chartData && (
+        <Pie
+          data={chartData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: 'Different Genres in Library'
+              }
             }
-          }
-        }}
-      />
+          }}
+        />
+      )}
     </div>
   );
-}
+};
+
 export default PieChart;
--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,112 +1,18 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { UserContext } from '../context/AuthContext'
-import EditProfile from './EditProfile'
-import PieChart from './PieChart'
-import { CategoryScale } from 'chart.js'
-import Chart from 'chart.js/auto'
-import '../Styles/Profile.css'
-
-Chart.register(CategoryScale)
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../context/AuthContext';
+import EditProfile from './EditProfile';
+import PieChart from './PieChart';
+import BarChart from './BarChart';
+import '../Styles/Profile.css';
+import '../Styles/Chart.css';
 
 const Profile = () => {
-  const { user, isLoggedIn, deleteAccount } = useContext(UserContext)
-  const [editing, setEditing] = useState(false)
-  const [chartData, setChartData] = useState(null)
+  const { user, isLoggedIn, deleteAccount } = useContext(UserContext);
+  const [editing, setEditing] = useState(false);
+//map through the users game_logs and return all play_time. Then use reduce to iterate through the games array and add up the total hours played
+const totalHoursPlayed = user?.game_logs?.map(log => log.play_time).reduce((acc, total) => acc + total, 0)
 
-
-  //map through the users game_logs and return all play_time. Then use reduce to iterate through the games array and add up the total hours played
-  const totalHoursPlayed = user?.game_logs?.map(log => log.play_time).reduce((acc, total) => acc + total, 0)
-
-  const totalGamesPlayed = user?.game_logs?.filter(log => log.status === 'Complete' || log.status === 'In Progress').length
-
-
-
-  
-  
-  useEffect(() => {
-    if (user && user.games) {
-      const games = user.games.filter(game => game.status !== 'Wishlist' && game.status !== 'Not Played')
-      const genres = games.map(game => game.genre)
-      // uniqueGenres is an array of unique genres. It's used to create the chart labels.
-      // const uniqueGenres = [...new Set(genres)]; //refactored from https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-      // ...new Set(genres) is used to remove duplicate genres from the genres array.
-      const chartData = {
-        labels: [],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: [],
-            borderColor: []
-          }
-        ]
-      };
-  
-      games.forEach(game => {
-        const genre = game.genre.name
-  
-        // Check if the genre already exists in the chart data
-        const existingGenreIndex = chartData.labels.indexOf(genre)
-        if (existingGenreIndex !== -1) { 
-          // Update the existing data value for the genre
-          chartData.datasets[0].data[existingGenreIndex]++
-        } else {
-          // Add a new genre to the chart data
-          chartData.labels.push(genre)
-          // Add a new data value for the genre
-          chartData.datasets[0].data.push(1)
-          // Choose color from the list based on index
-          const colorIndex = chartData.labels.length - 1
-          chartData.datasets[0].backgroundColor.push(
-            backgroundColor[colorIndex % backgroundColor.length]
-          )
-          chartData.datasets[0].borderColor.push(
-            borderColor[colorIndex % borderColor.length]
-          )
-        }
-      });
-  
-      setChartData(chartData);
-    }
-  }, [user]);
-  
-  const backgroundColor = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 99, 255, 0.2)',
-    'rgba(255, 159, 0, 0.2)',
-    'rgba(255, 205, 0, 0.2)',
-    'rgba(0, 128, 0, 0.2)',
-    'rgba(128, 128, 128, 0.2)',
-    'rgba(0, 0, 255, 0.2)',
-    'rgba(255, 0, 0, 0.2)',
-    'rgba(128, 0, 128, 0.2)',
-    'rgba(0, 255, 0, 0.2)'
-  ];
-  
-  const borderColor = [
-    'rgb(255, 99, 132)',
-    'rgb(255, 159, 64)',
-    'rgb(255, 205, 86)',
-    'rgb(75, 192, 192)',
-    'rgb(54, 162, 235)',
-    'rgb(153, 102, 255)',
-    'rgb(255, 99, 255)',
-    'rgb(255, 159, 0)',
-    'rgb(255, 205, 0)',
-    'rgb(0, 128, 0)',
-    'rgb(128, 128, 128)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 0)',
-    'rgb(128, 0, 128)',
-    'rgb(0, 255, 0)'
-  ];
-  
-  
-  
+const totalGamesPlayed = user?.game_logs?.filter(log => log.status === 'Complete' || log.status === 'In Progress').length
 
   if (!isLoggedIn) {
     return (
@@ -115,7 +21,7 @@ const Profile = () => {
           <h3>Please login to view your profile.</h3>
         </div>
       </div>
-    )
+    );
   } else if (editing) {
     return (
       <div className="profile-container">
@@ -181,15 +87,21 @@ const Profile = () => {
                 </p>
                 <p>
                   <label>Hours Played:</label>
-                  <span>{totalHoursPlayed}</span>
+                  <span>
+                    {totalHoursPlayed}
+                  </span>
                 </p>
               </>
             )}
           </div>
-          
           <button onClick={() => setEditing(true)}>Edit Profile</button>
           <hr />
-          {chartData && <PieChart chartData={chartData} />}
+        </div>
+        <div className="chart-container">
+          {user && user.games && <PieChart games={user.games} />}
+        </div>
+        <div className="chart-container">
+          {user && user.game_logs && <BarChart gameLogs={user.game_logs} />}
         </div>
       </div>
     );
