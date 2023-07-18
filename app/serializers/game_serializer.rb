1c51f6e6bfe94dd052148fe81da2db5d8fc4fa3e
stella <sftamesis13@gmail.com>
2023-07-18 09:36:12 -0700

    add game_logs to serializer to fix delete action

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :cover, :name, :platforms, :release_date, :involved_company, :player_perspective, :aggregated_rating, :aggregated_rating_count, :summary, :screenshots, :genre_id, :genre, :summary
+  attributes :id, :cover, :name, :platforms, :release_date, :involved_company, :player_perspective, :aggregated_rating, :aggregated_rating_count, :summary, :screenshots, :genre_id, :genre, :summary, :game_logs
 
   belongs_to :genre
 end
