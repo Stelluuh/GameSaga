064ca263a30f48d2e2faa85cc0440275d35e9f7b
stella <sftamesis13@gmail.com>
2023-07-18 09:16:56 -0700

    make changes so that user has option to use backup seed file IDGB_data.json instead of api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,126 @@
-require 'httparty'
-require 'json'
+# ----------------- USING .json data -----------------
 
-client_id = ENV['IGDB_CLIENT_ID']
-authorization = ENV['IGDB_AUTHORIZATION']
-limit = 500 # Maximum number of games to fetch per request
-offset = 0 # Initial offset value
+require 'json'
 
 def screenshot_url(hash)
   "https://images.igdb.com/igdb/image/upload/t_screenshot_big/#{hash}.jpg"
 end
 
-loop do
-  response = HTTParty.post('https://api.igdb.com/v4/games/', headers: {
-    'Client-ID' => client_id,
-    'Authorization' => "Bearer #{authorization}",
-    'Accept' => 'application/json'
-  }, body: "fields aggregated_rating, aggregated_rating_count, cover.url, first_release_date, genres.name, involved_companies.company.name, name, platforms.name, player_perspectives.name, summary, artworks.url, screenshots.url, keywords.name, websites.url; where genres != null & aggregated_rating > 85; limit #{limit}; offset #{offset};")
+# Load game data from JSON file
+games_data = JSON.parse(File.read('./db/IGDB_data.json'))
 
-  if response.code == 200
-    games_data = JSON.parse(response.body)
-    break if games_data.empty?
+games_data.each do |game|
+  screenshots = game.dig('screenshots')&.map { |screenshot| screenshot['url'] } || []
+  screenshot_hashes = screenshots.map { |url| url.split('/').last.split('.').first }
+  medium_screenshots = screenshot_hashes.map { |hash| screenshot_url(hash) }
 
-    games = games_data.map do |game|
-      screenshots = game.dig('screenshots')&.map { |screenshot| screenshot['url'] } || []
-      screenshot_hashes = screenshots.map { |url| url.split('/').last.split('.').first }
-      medium_screenshots = screenshot_hashes.map { |hash| screenshot_url(hash) }
+  game_data = {
+    name: game['name'],
+    cover: game.dig('cover', 'url'),
+    platforms: game.dig('platforms')&.map { |platform| platform['name'] },
+    release_date: game['first_release_date'].to_i,
+    involved_company: game.dig('involved_companies', 0, 'company', 'name'),
+    player_perspective: game.dig('player_perspectives', 0, 'name'),
+    aggregated_rating: game['aggregated_rating'].to_i,
+    aggregated_rating_count: game['aggregated_rating_count'].to_i,
+    summary: game['summary'],
+    genre_attributes: { name: game.dig('genres', 0, 'name') },
+    artworks: game.dig('artworks')&.map { |artwork| artwork['url'] } || [],
+    screenshots: medium_screenshots,
+    keywords: game.dig('keywords')&.map { |keyword| keyword['name'] } || [],
+    websites: { url: game.dig('websites', 0, 'url') }
+  }
 
-      {
-        name: game['name'],
-        cover: game.dig('cover', 'url'),
-        platforms: game.dig('platforms')&.map { |platform| platform['name'] },
-        release_date: game['first_release_date'].to_i,
-        involved_company: game.dig('involved_companies', 0, 'company', 'name'),
-        player_perspective: game.dig('player_perspectives', 0, 'name'),
-        aggregated_rating: game['aggregated_rating'].to_i,
-        aggregated_rating_count: game['aggregated_rating_count'].to_i,
-        summary: game['summary'],
-        genre_attributes: { name: game.dig('genres', 0, 'name') },
-        artworks: game.dig('artworks')&.map { |artwork| artwork['url'] } || [],
-        screenshots: medium_screenshots,
-        keywords: game.dig('keywords')&.map { |keyword| keyword['name'] } || [],
-        websites: { url: game.dig('websites', 0, 'url') }
-      }
-    end
+  genre_attributes = game_data.delete(:genre_attributes)
 
-    games.each do |game_data|
-      genre_attributes = game_data.delete(:genre_attributes)
+  # Creates a genre in the database if it doesn't exist
+  genre = Genre.find_or_create_by!(genre_attributes)
+  game = genre.games.build(game_data)
 
-      # creates a genre in the database if it doesn't exist
-      genre = Genre.find_or_create_by!(genre_attributes)
-      game = genre.games.build(game_data)
-
-
-
-      if game.valid?
-        game.save!
-        
-      else
-        puts "Skipping invalid game: #{game.name}"
-      end
-    end
-
-    offset += limit
+  if game.valid?
+    game.save!
   else
-    # Stop the loop if the response is not 200. This fixes DEPLOYMENT ISSUE.
-    puts "Error: #{response.code}"
-    puts response.body
-    break
+    puts "Skipping invalid game: #{game.name}"
   end
 end
 
 puts 'Seeding Completed!'
+
+
+
+
+# ----------------- USING IGDB API -----------------
+
+# require 'httparty'
+# require 'json'
+
+# client_id = ENV['IGDB_CLIENT_ID']
+# authorization = ENV['IGDB_AUTHORIZATION']
+# limit = 500 # Maximum number of games to fetch per request
+# offset = 0 # Initial offset value
+
+# def screenshot_url(hash)
+#   "https://images.igdb.com/igdb/image/upload/t_screenshot_big/#{hash}.jpg"
+# end
+
+# loop do
+#   response = HTTParty.post('https://api.igdb.com/v4/games/', headers: {
+#     'Client-ID' => client_id,
+#     'Authorization' => "Bearer #{authorization}",
+#     'Accept' => 'application/json'
+#   }, body: "fields aggregated_rating, aggregated_rating_count, cover.url, first_release_date, genres.name, involved_companies.company.name, name, platforms.name, player_perspectives.name, summary, artworks.url, screenshots.url, keywords.name, websites.url; where genres != null & aggregated_rating > 85; limit #{limit}; offset #{offset};")
+
+#   if response.code == 200
+#     games_data = JSON.parse(response.body)
+#     break if games_data.empty?
+
+#     games = games_data.map do |game|
+#       screenshots = game.dig('screenshots')&.map { |screenshot| screenshot['url'] } || []
+#       screenshot_hashes = screenshots.map { |url| url.split('/').last.split('.').first }
+#       medium_screenshots = screenshot_hashes.map { |hash| screenshot_url(hash) }
+
+#       {
+#         name: game['name'],
+#         cover: game.dig('cover', 'url'),
+#         platforms: game.dig('platforms')&.map { |platform| platform['name'] },
+#         release_date: game['first_release_date'].to_i,
+#         involved_company: game.dig('involved_companies', 0, 'company', 'name'),
+#         player_perspective: game.dig('player_perspectives', 0, 'name'),
+#         aggregated_rating: game['aggregated_rating'].to_i,
+#         aggregated_rating_count: game['aggregated_rating_count'].to_i,
+#         summary: game['summary'],
+#         genre_attributes: { name: game.dig('genres', 0, 'name') },
+#         artworks: game.dig('artworks')&.map { |artwork| artwork['url'] } || [],
+#         screenshots: medium_screenshots,
+#         keywords: game.dig('keywords')&.map { |keyword| keyword['name'] } || [],
+#         websites: { url: game.dig('websites', 0, 'url') }
+#       }
+#     end
+
+#     games.each do |game_data|
+#       genre_attributes = game_data.delete(:genre_attributes)
+
+#       # creates a genre in the database if it doesn't exist
+#       genre = Genre.find_or_create_by!(genre_attributes)
+#       game = genre.games.build(game_data)
+
+
+
+#       if game.valid?
+#         game.save!
+        
+#       else
+#         puts "Skipping invalid game: #{game.name}"
+#       end
+#     end
+
+#     offset += limit
+#   else
+#     # Stop the loop if the response is not 200. This fixes DEPLOYMENT ISSUE.
+#     puts "Error: #{response.code}"
+#     puts response.body
+#     break
+#   end
+# end
+
+# puts 'Seeding Completed!'
