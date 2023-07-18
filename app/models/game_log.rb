02bb413b318abf8a7e0586891f6753102f10e91e
stella <sftamesis13@gmail.com>
2023-07-18 09:40:03 -0700

    add abandoned validation for start and stop dates

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -3,15 +3,15 @@ class GameLog < ApplicationRecord
   belongs_to :user
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
-  validates :date_started, presence: true, if: :in_progress_or_completed?
+  validates :date_started, presence: true, if: :in_progress_or_completed_or_abandoned? 
   validates :date_stopped, presence: true, if: :abandoned?
   validates :date_completed, presence: true, if: :completed?
   validates :play_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   private
 
-  def in_progress_or_completed?
-    status.in?(['In Progress', 'Complete'])
+  def in_progress_or_completed_or_abandoned?
+    status.in?(['In Progress', 'Complete', 'Abandoned'])
   end
 
   def abandoned?
