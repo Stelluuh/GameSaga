4778a260de70a1a11d6fe2bbe36d02fcea1f7d1c
stella <sftamesis13@gmail.com>
2023-07-19 13:12:37 -0700

    update do most recent react Render

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
   <React.StrictMode>
   <Router>
     <App />
   </Router>
-</React.StrictMode>,  
-document.getElementById('root')
+</React.StrictMode>
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
 
 
 
