a33c9642b6f8b88e6f69637fe84a468a7d13185a
stella <sftamesis13@gmail.com>
2023-07-19 13:13:45 -0700

    Add editGameLog function from AuthContext to this component

--- client/src/Components/MyGameCard.js
+++ client/src/Components/MyGameCard.js
@@ -4,9 +4,34 @@ import EditGameLog from './EditGameLog';
 
 const MyGameCard = ({ game }) => {
   const { name, genre } = game;
-  const { deleteGameLog, editGameLog, user, errors } = useContext(UserContext);
+  const [logErrors, setLogErrors] = useState([])
   const [editingLogId, setEditingLogId] = useState(null);
+  
+  const { deleteGameLog, setUser, user } = useContext(UserContext);
 
+  const editGameLog = (editedLog) => {
+    fetch(`/game_logs/${editedLog.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(editedLog),
+    })
+        .then(response => response.json())
+        .then(updatedLog => {
+            if(updatedLog.errors) {
+                setLogErrors(updatedLog.errors)
+            } else
+            setLogErrors([])
+            setUser({ ...user, 
+              game_logs: user.game_logs.map(gameLog => 
+                gameLog.id === updatedLog.id ? updatedLog : gameLog
+                ), 
+              games: user.games.map(game => 
+                game.id === updatedLog.game_id ? updatedLog.game : game
+                )
+            })
+        })
+}
+  
   const handleDeleteGameLog = (gameLogId) => {
     deleteGameLog(gameLogId);
   };
@@ -20,46 +45,20 @@ const MyGameCard = ({ game }) => {
   };
 
   const handleSaveGameLog = (editedLog) => {
+
     editGameLog(editedLog);
     setEditingLogId(null);
+
   };
 
-  const displayErrors = () => {
-    return errors.map((error) => <p style={{color: 'red'}}key={error}>{error}</p>);
-  }
-
+  
+  
   const gameLogs = user.game_logs?.filter((log) => log.game_id === game.id);
-
- 
-  const displayGameLogs = () => {
-    return gameLogs?.map((log) => (
-      <div key={log.id}>
-        {editingLogId === log.id ? (
-          <EditGameLog
-            gameLog={log}
-            onCancel={handleCancelEdit}
-            onSave={handleSaveGameLog}
-          />
-        ) : (
-          <>
-            <p>Status: {log.status ? log.status : null}</p> 
-            {log.rating ? <p>Rating: {log.rating}</p> : null}
-            {log.play_time ? <p>Hours Played: {log.play_time}</p> : null}
-            {log.date_started ? <p>Date Started: {log.date_started}</p> : null}
-            {log.date_stopped ? <p>Date Stopped: {log.date_stopped}</p> : null}
-            {log.date_completed ? <p>Date Completed: {log.date_completed}</p> : null}
-            {displayErrors()}
-            <button className="btn btn-danger" onClick={() => handleDeleteGameLog(log.id)}>Delete</button>
-            <button className="btn btn-primary" onClick={() => handleEditGameLog(log.id)}>Edit</button>
-          </>
-        )}
-      </div>
-    ));
-  };
-
-
-
-//---------- 1st attempt at rendering fields based on status ----------//
+  
+  
+  const displayErrors = () => {
+    return logErrors.map((error) => < tr style={{color: 'red'}}key={error}>{error}</tr>);
+  }
 
 
   return (
@@ -68,7 +67,7 @@ const MyGameCard = ({ game }) => {
         <h5 className="card-title text-primary">{name}</h5>
         <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p>
         <div>
-          <p className="font-italic">Game Logs:</p>
+          <p className="font-italic">Game Log:</p>
           <table className="table table-dark">
             <thead>
               <tr>
@@ -118,23 +117,4 @@ const MyGameCard = ({ game }) => {
 export default MyGameCard;
 
 
-//---------- 2nd attempt at rendering fields based on status ----------//
 
-
-//   return (
-//     <div className="card bg-dark text-light mb-3">
-//       <div className="card-body">
-//         <h5 className="card-title text-primary">{name}  ({genre.name})</h5>
-//         {/* <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p> */}
-//         <div>
-//           <div className="list-group">
-//             {displayGameLogs()}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
-// export default MyGameCard;
