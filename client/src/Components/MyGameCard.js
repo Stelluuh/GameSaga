cd517aaedf1bfc5207398845aa93da2a811b165b
stella <sftamesis13@gmail.com>
2023-07-18 19:54:31 -0700

    2nd attempt on rendering Edit game log to display specific inputs

--- client/src/Components/MyGameCard.js
+++ client/src/Components/MyGameCard.js
@@ -62,79 +62,79 @@ const MyGameCard = ({ game }) => {
 //---------- 1st attempt at rendering fields based on status ----------//
 
 
-//   return (
-//     <div className="card bg-dark text-light mb-3">
-//       <div className="card-body">
-//         <h5 className="card-title text-primary">{name}</h5>
-//         <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p>
-//         <div>
-//           <p className="font-italic">Game Logs:</p>
-//           <table className="table table-dark">
-//             <thead>
-//               <tr>
-//                 <th>Status</th>
-//                 <th>Rating</th>
-//                 <th>Hours Played</th>
-//                 <th>Date Start</th>
-//                 <th>Date Stopped</th>
-//                 <th>Date Completed</th>
-//                 <th>Action</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {gameLogs?.map((log) => (
-//                 <tr key={log.id}>
-//                   {editingLogId === log.id ? (
-//                     <EditGameLog
-//                       gameLog={log}
-//                       onCancel={handleCancelEdit}
-//                       onSave={handleSaveGameLog}
-//                     />
-//                   ) : (
-//                     <>
-//                       <td>{log.status}</td>
-//                       <td>{log.rating}</td>
-//                       <td>{log.play_time}</td>
-//                       <td>{log.date_started}</td>
-//                       <td>{log.date_stopped}</td>
-//                       <td>{log.date_completed}</td>
-//                       <td>
-//                         <button className="btn btn-danger" onClick={() => handleDeleteGameLog(log.id)}>Delete</button>
-//                         <button className="btn btn-primary" onClick={() => handleEditGameLog(log.id)}>Edit</button>
-//                       </td>
-//                     </>
-//                   )}
-//                 </tr>
-//               ))}
-//               {displayErrors()}
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MyGameCard;
-
-
-//---------- 2nd attempt at rendering fields based on status ----------//
-
-
   return (
     <div className="card bg-dark text-light mb-3">
       <div className="card-body">
-        <h5 className="card-title text-primary">{name}  ({genre.name})</h5>
-        {/* <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p> */}
+        <h5 className="card-title text-primary">{name}</h5>
+        <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p>
         <div>
-          <div className="list-group">
-            {displayGameLogs()}
-          </div>
+          <p className="font-italic">Game Logs:</p>
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th>Status</th>
+                <th>Rating</th>
+                <th>Hours Played</th>
+                <th>Date Start</th>
+                <th>Date Stopped</th>
+                <th>Date Completed</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gameLogs?.map((log) => (
+                <tr key={log.id}>
+                  {editingLogId === log.id ? (
+                    <EditGameLog
+                      gameLog={log}
+                      onCancel={handleCancelEdit}
+                      onSave={handleSaveGameLog}
+                    />
+                  ) : (
+                    <>
+                      <td>{log.status}</td>
+                      <td>{log.rating}</td>
+                      <td>{log.play_time}</td>
+                      <td>{log.date_started}</td>
+                      <td>{log.date_stopped}</td>
+                      <td>{log.date_completed}</td>
+                      <td>
+                        <button className="btn btn-danger" onClick={() => handleDeleteGameLog(log.id)}>Delete</button>
+                        <button className="btn btn-primary" onClick={() => handleEditGameLog(log.id)}>Edit</button>
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+              {displayErrors()}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
   );
 };
 
-
 export default MyGameCard;
+
+
+//---------- 2nd attempt at rendering fields based on status ----------//
+
+
+//   return (
+//     <div className="card bg-dark text-light mb-3">
+//       <div className="card-body">
+//         <h5 className="card-title text-primary">{name}  ({genre.name})</h5>
+//         {/* <p className="card-text">Genre: <span className="font-weight-bold">{genre.name}</span></p> */}
+//         <div>
+//           <div className="list-group">
+//             {displayGameLogs()}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default MyGameCard;
