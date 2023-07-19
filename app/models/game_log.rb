4403481ab85abb9277d1edc972aab1d36c71fc98
stella <sftamesis13@gmail.com>
2023-07-18 18:34:27 -0700

    create custom validations for dates to make sure dates are valid depending on status

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -4,26 +4,59 @@ class GameLog < ApplicationRecord
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
   validates :date_started, presence: true, if: :in_progress_or_completed_or_abandoned?
-  validates :date_stopped, presence: true, if: :abandoned?
+  validates :date_stopped, presence: true, if: :abandoned? 
   validates :date_completed, presence: true, if: :completed?
-  validates :date_started, :date_stopped, :date_completed, date: true, if: :valid_dates?
   validates :play_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validate :date_started_before_date_stopped
+  validate :date_started_before_date_completed
+  validate :date_started_valid
+  validate :date_stopped_valid
+  validate :date_completed_valid
 
+  
   private
-
+  
   def in_progress_or_completed_or_abandoned?
     status.in?(['In Progress', 'Complete', 'Abandoned'])
   end
-
+  
   def abandoned?
     status == 'Abandoned'
   end
-
+  
   def completed?
     status == 'Complete'
   end
-
-  def valid_dates?
-    date_started < date_stopped && date_started < date_completed
+  
+  def date_started_before_date_stopped
+    if date_started.present? && date_stopped.present? && date_started > date_stopped
+      errors.add(:date_started, "must be before date stopped")
+    end
   end
+
+  def date_started_before_date_completed
+    if date_started.present? && date_completed.present? && date_started > date_completed
+      errors.add(:date_started, "must be before date completed")
+    end
+  end
+
+  #q: how to make sure that all dates selected is not in the future?
+  def date_started_valid
+    if date_started.present? && date_started > Date.today
+      errors.add(:date_started, "must be before today")
+    end
+  end
+
+  def date_stopped_valid
+    if date_stopped.present? && date_stopped > Date.today
+      errors.add(:date_stopped, "must be before today")
+    end
+  end
+
+  def date_completed_valid
+    if date_completed.present? && date_completed > Date.today
+      errors.add(:date_completed, "must be before today")
+    end
+  end
+
 end
