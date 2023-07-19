af5e92f19033e2aac8b7f06936979241bd89bdf0
stella <sftamesis13@gmail.com>
2023-07-18 18:35:08 -0700

    small structure edit to push end to match margin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::API
       @current_user = User.find_by(id: session[:user_id])
 
       render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-  end
+    end
 
 
 end
