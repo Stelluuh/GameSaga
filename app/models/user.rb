ed5e969ac07fe1d287e2651de08f7fa70d3b7b4f
stella <sftamesis13@gmail.com>
2023-07-20 19:20:58 -0700

    add case_sensitive validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    validates :username, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :password, :password_confirmation, presence: true, length: { minimum: 8 }
 end
