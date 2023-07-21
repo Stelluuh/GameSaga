02d1604b153cfaf8107927b1326043153fc7d1b0
stella <sftamesis13@gmail.com>
2023-07-20 19:14:40 -0700

    add validations to creating and editing profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,8 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :age, numericality: { only_integer: true, greater_than: 0 }
-  # validates :avatar, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :avatar, presence: true
   validates :bio, length: { maximum: 500 }
-  validates :total_games_played, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :hours_played, numericality: { greater_than_or_equal_to: 0 }
+  # validates :favorite_genre, presence: true
 end
